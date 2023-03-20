b4851da87fc3de51bf3c8d314244c8df9157f3d9
hitome0123 <121650002+hitome0123@users.noreply.github.com>
2023-03-20 18:25:10 +0800

    Update ERC20 functions.sol

--- ERC20 functions.sol
+++ ERC20 functions.sol
@@ -1,12 +1,34 @@
-//SPDX-License-Indentifier:MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
 //ERC20继承IERC20的所有属性和功能
 contract ERC20 is IERC20{
   //变量定义。「在此contract中事先写定token的信息，如果不需要可直接把=“xxx”去掉即可」
   string public name="TalentedUs";//定义token全名name，可见性为public
   string public symbol="TDUS";//定义token简称symbol
   uint8 public decimals=18;//定义token精度decimals
-  uint256 public totalSupply=1000000;//定义供应量totalSupply
+  uint public totalSupply;//定义供应量totalSupply
   address public owner;//定义代币的所有方
   mapping(address=>uint256) public balanceOf;//定义查询余额的映射变量，根据addresss地址查到余额uint256
   mapping(address=>mapping(address=>uint256)) public allowance;//定义查询授权的额度，根据授权方address(第一个)映射到「被授权方address(第二个)映射到额度数量uint256」
@@ -15,9 +37,9 @@ contract ERC20 is IERC20{
   owner=msg.sender;}//代币的所有者是合约的发起地址
   //当合约被部署到区块链时，这个构造函数只会被调用一次。通过将owner变量设置为消息发送者的地址，合约的所有者将是部署合约的帐户。这是在以太坊智能合约中设置合约所有者的常见模式。
   
-  modifier OnlyOwner(){
-  require(msg.sender==owner,"Only owner can mint")
-  _;}
+  modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner can mint");
+        _;}
   
   //转账函数 传入收款地址，金额 ，返回bool
   function transfer(address recipient,uint256 amount) external returns(bool){
@@ -29,8 +51,8 @@ contract ERC20 is IERC20{
   //授权函数
   function approve(address recipient,uint256 amount)external returns(bool){
     allowance[msg.sender][recipient]=amount;//查询授权额度，格式为allowance[发起方地址][接收方地址]
-    emit Approval(msg.sender,recipient,amount)//返回授权事件，三个参数，发起方，接收方，数量
-    returns true;
+    emit Approval(msg.sender,recipient,amount);//返回授权事件，三个参数，发起方，接收方，数量
+    return true;
     }
   //授权方转账函数
   function transferFrom(address sender,address recipient,uint256 amount )external returns(bool){
@@ -40,15 +62,15 @@ contract ERC20 is IERC20{
      return true;
     }
 //铸造token函数
-function mint(uint256 amount) external returns(bool){
+function mint(uint256 amount) external onlyOwner(){
   balanceOf[msg.sender]+=amount;
-  totalSupply[msg.sender]+=amount;
+  totalSupply+=amount;
   emit Transfer(address(0),msg.sender,amount);
   }
  //销毁token函数
- function burn(uint256 amount)external returns(bool){
+ function burn(uint256 amount)external onlyOwner(){
   balanceOf[msg.sender]-=amount;
-  totalSupply[msg.sender]-=amount;
+  totalSupply-=amount;
   emit Transfer(msg.sender,address(0),amount);}
   
 }
