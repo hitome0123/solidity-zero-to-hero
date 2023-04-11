2c049d55c94a15ef2cb475832e547ea11bbc97e6
hitome0123 <121650002+hitome0123@users.noreply.github.com>
2023-04-11 14:55:58 +0800

    Update Proxiable.sol

--- Proxiable.sol
+++ Proxiable.sol
@@ -4,18 +4,20 @@ pragma solidity ^0.8.1;
 
 contract Proxiable {
     // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
-
+    //定义名为updateCodeAddress()的函数，传入newAddress为参数
     function updateCodeAddress(address newAddress) internal {
         require(
             bytes32(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7) == Proxiable(newAddress).proxiableUUID(),
             "Not compatible"
-        );
+        );//要求newAddress的proxiableUUID()必须等于bytes32(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
         assembly { // solium-disable-line
             sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, newAddress)
-        }
+        }//不满足上述条件，显示错误信息“不兼容”
     }
 
     function proxiableUUID() public pure returns (bytes32) {
         return 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7;
     }
 } 
+//0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7 用于updateCodeAddress验证正在设置
+的新地址是否与当前合约具有相同的UUID(通用唯一标识符)，这用于确保正在设置的新合约与当前合约兼容，并且可以用作代理。
